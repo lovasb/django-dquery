cbe49a03cd91bd52e41da261d2a568a634437596
Lovas Bence <bence.lovas@maven7.hu>
2013-03-29 10:54:49 +0100

    Simplify client side code

--- dquery/templates/dquery/dquery.core.js
+++ dquery/templates/dquery/dquery.core.js
@@ -1,36 +1,12 @@
 <script type="text/javascript">
-var DQuery = {
-    ajax: function(args) {
-        url_args = typeof args.url_args !== 'undefined' ? args.url_args : [];
-        arguments = {'url': '{% url  dquery-urlreverse %}?' + $.param({'url': args.url, 'url_args': url_args}) }
-        if ('data' in args) {
-            arguments['type'] = 'POST'
-            arguments['data'] = args.data
-        } else {
-            arguments['type'] = 'GET'
-        }
-        if ('success' in args) {
-            arguments['success'] = function(data, textStatus, jqXHR) {
-                args.success(data);
-            }
-        }
-        if ('error' in args) {
-            arguments['error'] = function(data, textStatus, jqXHR) {
-                args.error(data);
-            }
-        }
-        $.ajax(arguments);
-    },
-    load: function(args) {
-        url_args = typeof args.url_args !== 'undefined' ? args.url_args : [];
-        arguments = {'url': '/dquery/?' + $.param({'url': args.url, 'url_args': url_args}) }
-        if (($.isArray(args.data)) && (args.data.length > 0)) {
-            arguments['type'] = 'POST'
-            arguments['data'] = args.data;
-        } else {
-            arguments['type'] = 'GET'
-        }
-        $(args.selector).load(arguments);
+	var DQuery = {
+	    ajax: function(args) {
+	        url_args = typeof args['url_args'] !== 'undefined' ? args['url_args'] : [];
+	        args['url'] = '{% url  dquery-urlreverse %}?' + $.param({'url_name': args['url_name'], 'url_args':url_args})
+	        //console.log('{% url  dquery-urlreverse %}?' + $.param({'url_name': args['url_name'], 'url_args':url_args}));
+	        delete args['url_name']
+	        delete args['url_args']
+		    $.ajax(args);
+	    }
     }
-}
 </script>
