f975e2b5be5a611d4577012cca9a899fb7b95341
Lovas Bence <lovas.bence@codehouse.hu>
2013-06-23 17:40:23 +0200

    Update to django 1.5

--- dquery/templates/dquery/dquery.core.js
+++ dquery/templates/dquery/dquery.core.js
@@ -2,8 +2,8 @@
 	var DQuery = {
 	    ajax: function(args) {
 	        url_args = typeof args['url_args'] !== 'undefined' ? args['url_args'] : [];
-	        args['url'] = '{% url  dquery-urlreverse %}?' + $.param({'url_name': args['url_name'], 'url_args':url_args})
-	        //console.log('{% url  dquery-urlreverse %}?' + $.param({'url_name': args['url_name'], 'url_args':url_args}));
+	        args['url'] = '{% url  'dquery-urlreverse' %}?' + $.param({'url_name': args['url_name'], 'url_args':url_args})
+	        //console.log('{% url  'dquery-urlreverse' %}?' + $.param({'url_name': args['url_name'], 'url_args':url_args}));
 	        delete args['url_name']
 	        delete args['url_args']
 		    $.ajax(args);
